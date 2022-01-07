2104957f421d549a80313bded762701c1b99af82
Thomas Liebert <thomas.d.liebert@gmail.com>
2022-01-07 14:42:26 -0600

    implemented function adding data based on list postition

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './styles.css';
 
+function addPositionToChildren (container) {
+
+    function returnArrayOfChildren(parentEl) {
+        return Array.from(parentEl.children)
+    }
+
+    let arrayChildren = returnArrayOfChildren(container)
+    
+    function addDatakeyByIndex(element, index) {
+        // console.log(typeof element, element);
+        element.setAttribute("data-position", index)
+    }
+    
+    function addDataAttributes(arr) {
+        console.log(typeof arr, arr)
+        arr.forEach(addDatakeyByIndex);
+    }
+
+    addDataAttributes(arrayChildren)
+
+}
+
+let listcontainer = document.querySelector("#menucontainer")
+
+addPositionToChildren(listcontainer)
+
